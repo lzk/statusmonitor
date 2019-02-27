d09c1ca46ff1842951071b0030e5b63d31623104
Jacky Liang <jacky@Jackys>
2019-02-27 13:36:41 +0800

    update error strings

--- tomcat/enterpassword.cpp
+++ tomcat/enterpassword.cpp
@@ -8,6 +8,13 @@ EnterPassword::EnterPassword(QWidget *parent) :
     ui->setupUi(this);
     ui->buttonBox->button(QDialogButtonBox::Ok)->setText("确定");
     ui->buttonBox->button(QDialogButtonBox::Cancel)->setText("取消");
+
+//    QRegExp regexp;
+//    regexp.setPattern("^?[0-9a-fA-F]{8,16}$");
+    QRegExp regexp("^[\\x0020-\\x007e]{8,16}$");
+    QValidator *validator = new QRegExpValidator(regexp, this);
+    ui->lineEdit->setValidator(validator);
+
 }
 
 EnterPassword::~EnterPassword()
