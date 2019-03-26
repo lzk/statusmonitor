b8eb4c715e790c7b6bab5eea54821362f51c9ca5
Jacky Liang <jacky@Jackys>
2019-03-26 08:48:53 +0800

    12 test fail,transfer for fix

--- cpath/uiconfig.cpp
+++ cpath/uiconfig.cpp
@@ -94,7 +94,7 @@ void UIConfig::initConfig()
     getpidvid = _getpidvid;
 
     log_app_name = "lenovo cpath";
-    app_version = "1.0.0.11";
+    app_version = "1.0.0.12beta";
     log_init();
 }
 #include <QFile>
